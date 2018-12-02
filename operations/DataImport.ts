5e117212b0bc2559e1525e2f1108f8168eda9dce
James Clarke <james@jamesclarke.net>
2018-12-02 09:32:10 -0500

    Add DataImport for Flickr photo pull
    Include temporary endpoint for development.

---
+++ operations/DataImport.ts
@@ -0,0 +1,138 @@
+import * as Flickr from 'flickr-sdk';
+import * as mongodb from 'mongodb';
+
+interface Stream {
+  service_key: string,
+  oauth_token: string,
+  oauth_token_secret: string,
+  service: string,
+  last_checked: Date,
+  created: Date,
+  email: string
+}
+
+interface FlickrApiPhoto {
+  id: string,
+  owner: string,
+  secret: string,
+  server: string,
+  farm: number,
+  title: string,
+  ispublic: number,
+  isfriend: number,
+  isfamily: number,
+  description: { _content: string },
+  dateupload: number,
+  datetaken: string,
+  tags: string,
+  media: string,
+}
+
+interface FlickrPhoto {
+  service: string,
+  service_key: string,
+  item_key: string,
+  date_uploaded: Date,
+  date_taken: Date,
+  date_fetched: Date,
+  farm: number,
+  server: string,
+  secret: string,
+  type: string
+}
+
+export class DataImport {
+  stream: Stream;
+  mongo: mongodb.MongoClient;
+  dryRun: boolean;
+
+  constructor(stream: Stream, mongo: mongodb.MongoClient, dryRun: boolean = false) {
+    this.stream = stream;
+    this.mongo = mongo;
+    this.dryRun = dryRun;
+  }
+
+  async import(since: Date = this.stream.last_checked, collection = this.mongo.db("digestif").collection("items")) {
+    const photos = await this.getFlickrPhotos(since);
+    if (!this.dryRun) {
+      const result = await collection.insertMany(photos);
+      return result.insertedCount;
+    } else {
+      return photos.length;
+    }
+  }
+
+  async getFlickrPhotos(since: Date) {
+    const apiKey = process.env.FLICKR_API_KEY;
+    const apiSecret = process.env.FLICKR_API_SECRET;
+    const flickr = new Flickr(Flickr.OAuth.createPlugin(
+      apiKey,
+      apiSecret,
+      this.stream.oauth_token,
+      this.stream.oauth_token_secret));
+    const query: { user_id: string, extras: string, min_upload_date: number, page?: number } = {
+      user_id: this.stream.service_key,
+      extras: "date_upload, date_taken, description, media, tags, url_h, url_k",
+      min_upload_date: Math.floor(since.getTime() / 1000)
+    }
+    const photos: FlickrPhoto[] = [];
+    let data: any = {};
+    // Flickr API doesn't provide any sorting options for getPhotos. Load all photos into memory before writing
+    // out to mongo
+    do {
+      data = (await flickr.people.getPhotos(query)).body;
+      const pagePhotos = (data.photos.photo as [any])
+        .filter(photo => this.keepPhoto(photo))
+        .map((photo) => this.transform(photo));
+      photos.concat(pagePhotos);
+      query.page = data.page + 1;
+    } while (data.stat === 'ok' && data.pages <= data.page)
+    return photos;
+  }
+
+  keepPhoto(photo: FlickrApiPhoto): boolean {
+    const visible = photo.ispublic == 1 || photo.isfamily == 1 || photo.isfriend == 0;
+    const ignoreTag = photo.tags.indexOf("digestif:ignore=true") != -1;
+    return visible && !ignoreTag;
+  }
+
+  transform(photo: FlickrApiPhoto): FlickrPhoto {
+    return {
+      service: "flickr",
+      service_key: photo.owner,
+      item_key: photo.id,
+      date_uploaded: new Date(Number(photo.dateupload) * 1000),
+      date_taken: new Date(photo.datetaken),
+      date_fetched: new Date(),
+      farm: photo.farm,
+      server: photo.server,
+      secret: photo.secret,
+      type: photo.media
+    }
+  }
+
+  static async importAll(mongo = this.createMongoClient(), dryRun: boolean = false) {
+    await mongo.connect();
+    const streams = mongo.db("digestif").collection("streams");
+    const cursor = streams.find({});
+    const results: {} = {};
+    while (await cursor.hasNext()) {
+      const stream = await cursor.next() as Stream;
+      if (stream.service === "flickr") {
+        const data = await new DataImport(stream, mongo, dryRun).import();
+        results[stream.service_key] = data;
+      }
+    }
+    mongo.close();
+    return results;
+  }
+
+  static createMongoClient() {
+    const user = process.env.MONGO_USER;
+    const password = process.env.MONGO_PASSWORD;
+    const server = process.env.MONGO_SERVER;
+    const uri: string = `mongodb+srv://${user}:${password}@${server}/test?retryWrites=true`;
+    const mongo = new mongodb.MongoClient(uri, { useNewUrlParser: true });
+    return mongo;
+  }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as next from 'next';
 import * as digestif from './digestif';
+import { DataImport } from '../operations/DataImport';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -11,15 +12,19 @@ app.prepare().then(() => {
 
   server.get('/digest/:id', (req, res) => {
     digestif.getDigest(req.params.id).then(items => (app.render(req, res, '/digest', { preloaded: items })));
-  })
+  });
+
+  server.get('/flickr', (_, res) => {
+    DataImport.importAll(DataImport.createMongoClient(), true).then(result => res.send(result)).catch((err) => res.send({error:err}));
+  });
 
   // Fall-back on other next.js assets.
   server.get('*', (req, res) => {
     return handle(req, res);
-  })
+  });
 
   server.listen(3000, (err: any) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000')
-  })
+  });
 })
