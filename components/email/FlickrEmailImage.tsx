ad9b80f99b70f5088ac8fe39e6cde3e199f7cd3a
James Clarke <james@jamesclarke.net>
2019-11-14 19:11:02 -0500

    Email only show title  and description if  present

--- components/email/FlickrEmailImage.tsx
+++ components/email/FlickrEmailImage.tsx
@@ -10,7 +10,7 @@ interface FlickrEmailImageProps extends FlickrPhotoProps {
 export class FlickrEmailImage extends FlickrImage<FlickrEmailImageProps> {
   public render() {
     return (
-      <MjmlColumn width="100%" vertical-align="top">
+      <MjmlColumn width="100%" vertical-align="top" padding-bottom="10px">
         <MjmlImage
           width="600px"
           src={this.imgUrlSize("b")}
@@ -20,29 +20,33 @@ export class FlickrEmailImage extends FlickrImage<FlickrEmailImageProps> {
           padding="0"
           border-radius="0px"
         />
-        <MjmlText
-          font-size="17px"
-          align="left"
-          line-height="18.7px"
-          padding="10px 10px 0px 10px"
-        >
-          <span>{this.props.title}</span>
-        </MjmlText>
-        <MjmlText
-          font-size="14px"
-          align="left"
-          line-height="15.4px"
-          padding="10px"
-        >
-          <p>
-            {this.props.description.split("\n").map((item, key) => (
-              <span key={key}>
-                {item}
-                <br />
-              </span>
-            ))}
-          </p>
-        </MjmlText>
+        {this.props.title && (
+          <MjmlText
+            font-size="17px"
+            align="left"
+            line-height="18.7px"
+            padding="10px 10px 0px 10px"
+          >
+            <span>{this.props.title}</span>
+          </MjmlText>
+        )}
+        {this.props.description && (
+          <MjmlText
+            font-size="14px"
+            align="left"
+            line-height="15.4px"
+            padding="10px"
+          >
+            <p>
+              {this.props.description.split("\n").map((item, key) => (
+                <span key={key}>
+                  {item}
+                  <br />
+                </span>
+              ))}
+            </p>
+          </MjmlText>
+        )}
       </MjmlColumn>
     );
   }
